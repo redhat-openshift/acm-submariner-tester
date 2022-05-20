0596611b99f9a34d73deaf9f57ae2059fd72614d
nmanos <nmanos@redhat.com>
2022-05-20 17:17:14 +0300

    Add to junit skipped system tests, if last status got error
    Signed-off-by: nmanos <nmanos@redhat.com>

--- lib/sh2ju.sh
+++ lib/sh2ju.sh
@@ -7,7 +7,7 @@
 ### Licensed under the Apache License, Version 2.0.
 ### You may obtain a copy of it at
 ### http://www.apache.org/licenses/LICENSE-2.0
-
+###
 ###
 ### A library for shell scripts which creates reports in jUnit format.
 ### These reports can be used in Jenkins, or any other CI.
@@ -23,6 +23,7 @@
 ###             -ierror="RegExp"       : same as -error but case insensitive
 ###             -output="OutputDir"    : path of output directory, defaults to "./results"
 ###             -file="OutputFile"     : name of output file, defaults to "junit.xml"
+###             -status="StatusFile"   : Continue/Skip further tests according to status file: 0 (OK), 1 (Critical failure: Skip rest), 2 (Failed but continue)
 ###             -index                 : add incremental test index (e.g. for alphabetical sort in Jenkins)
 ###     - Junit reports are left in the folder 'result' under the directory where the script is executed.
 ###     - Configure Jenkins to parse junit files from the generated folder
@@ -57,9 +58,10 @@ else
   exit 1
 fi
 
-# Function to execute command (like bash eval method), witch allows catching seg-faults and use tee
+# Function to execute command (like bash eval method), which allows catching seg-faults and use tee
+# It saves output to temporary files: stdout > ${outf}, stderr > ${errf}, exit code > ${returnf}
 function eVal() {
-  # execute the command, temporarily swapping stderr and stdout so they can be tee'd to separate files,
+  # Execute the command, temporarily swapping stderr and stdout so they can be tee'd to separate files,
   # then swapping them back again so that the streams are written correctly for the invoking process
   echo 0 > "${returnf}"
   (
@@ -75,8 +77,8 @@ function eVal() {
 
 # TODO: Use this function to clean old test results (xmls)
 function juLogClean() {
-  echo "+++ sh2ju removing old junit reports from: ${juDIR} "
-  find "${juDIR}" -maxdepth 1 -name "${juFILE}" -delete
+  echo "+++ sh2ju removing old junit reports from: ${juDir} "
+  find "${juDir}" -maxdepth 1 -name "${juFile}" -delete
 }
 
 # Function to remove special characters and ansi colors from a text file
@@ -108,7 +110,7 @@ function juLog() {
 
   # Initialize testsuite attributes
   dateTime="$(which gdate 2>/dev/null || which date || :)"
-  asserts=00; failures=0; suiteDuration=0; # content=""
+  asserts=00; failures=0; skipped=0; suiteDuration=0; # content=""
   export testIndex=$(( testIndex+1 ))
 
   # parse arguments
@@ -119,8 +121,9 @@ function juLog() {
       -name=*)   name="$(echo "$1" | ${SED} -e 's/-name=//')";   shift;;
       -ierror=*) ereg="$(echo "$1" | ${SED} -e 's/-ierror=//')"; icase="-i"; shift;;
       -error=*)  ereg="$(echo "$1" | ${SED} -e 's/-error=//')";  shift;;
-      -output=*) juDIR="$(echo "$1" | ${SED} -e 's/-output=//')";  shift;;
-      -file=*)   juFILE="$(echo "$1" | ${SED} -e 's/-file=//')";  shift;;
+      -output=*) juDir="$(echo "$1" | ${SED} -e 's/-output=//')";  shift;;
+      -file=*)   juFile="$(echo "$1" | ${SED} -e 's/-file=//')";  shift;;
+      -status=*) statusFile="$(echo "$1" | ${SED} -e 's/-status=//')";  shift;;
       -index)    sortTests="$(echo "$1" | ${SED} -e 's/-index/TRUE/')";  shift;;
       *)         ya=1;;
     esac
@@ -134,26 +137,30 @@ function juLog() {
   suiteTitle="${class//_/ }"
   suiteTitle="${suiteTitle[@]^}"
 
-  # set output directory as ./results , if it was not given
-  juDIR="${juDIR:-$(pwd)/results}"
+  # Set output directory as ./results , if it was not given
+  juDir="${juDir:-$(pwd)/results}"
 
-  # set output file name as class name, if it was not given
-  juFILE="${juFILE:-${class}_junit.xml}"
+  # Set output file name as class name, if it was not given
+  juFile="${juFile:-${class}_junit.xml}"
 
-  # create output directory
-  mkdir -p "${juDIR}" || exit
-  # use first arg as name if it was not given
+  # Set overall suite status, according to the content of status file
+  # Should be either 0 (OK to continue tests), 1 (Critical failure: Skip rest of tests), 2 (Failed but continue)
+  testSuiteStatus="$([[ ! -s "$statusFile" ]] || cat "$statusFile")"
+
+  # Create output directory
+  mkdir -p "${juDir}" || exit
+  # Use first arg as name if it was not given
   if [[ -z "${name}" ]]; then
     name="${asserts}-$1"
     shift
   fi
 
-  if [[ ! -e "${juDIR}/${juFILE}" ]]; then
+  if [[ ! -e "${juDir}/${juFile}" ]]; then
     # no Junit file exists. Adding a new file
-    cat <<EOF > "${juDIR}/${juFILE}"
+    cat <<EOF > "${juDir}/${juFile}"
 <?xml version="1.0" encoding="UTF-8"?>
 <testsuites>
-    <testsuite name="${suiteTitle}" tests="0" assertions="" failures="0" errors="0" time="0">
+    <testsuite name="${suiteTitle}" tests="0" assertions="" failures="0" errors="0" skipped="0" time="0">
     </testsuite>
 </testsuites>
 EOF
@@ -171,7 +178,8 @@ EOF
   echo "+++ sh2ju running case${testIndex:+ ${testIndex}}: ${class}.${name} "
   echo "+++ sh2ju working directory: $(pwd)"
   echo "+++ sh2ju command: ${cmd}"
-  # To print +++ sh2ju debugs also into ${outf}, add:   | tee -a ${outf}
+  echo "+++ sh2ju test suite status: ${testSuiteStatus}"
+  # To print +++ sh2ju (debuging lines) into ${outf}, you can add: | tee -a ${outf}
 
   # Clear content of the temporary files
   : > "${outf}"
@@ -181,12 +189,33 @@ EOF
   # Save datetime before executing the command
   testStartTime="$(${dateTime} +%s.%N)"
 
-  ### Calling eVal() function that will run the command and save output to the temporary files:
-  # Function stdout > ${outf}
-  # Function stderr > ${errf}
-  # Function return (exit) code > ${returnf}
-  eVal "${cmd}"
-  returnCode="$([[ -s "$returnf" ]] && cat "$returnf" || echo "0")"
+  ### If testSuiteStatus is not 1: Call eVal() to execute the test, and set suite status result 
+  if [[ "$testSuiteStatus" != 1 ]] ; then
+    # Run command and set the test return code, based on the command exit code
+    eVal "${cmd}"
+    returnCode="$([[ -s "$returnf" ]] && cat "$returnf" || echo "0")"
+
+    if [[ "${returnCode}" != 0 ]] ; then
+      # Command failed
+      if [[ -f "$statusFile" ]] ; then
+        # If suite status is empty - set status file to: 1 (Critical failure)
+        [[ -n "$testSuiteStatus" ]] || echo 1 > "$statusFile"
+        # If suite status is 0 (Pass) - set status file to: 2 (Failed but continue)
+        [[ "$testSuiteStatus" != 0 ]] || echo 2 > "$statusFile"
+      fi
+      testCaseStatus=FAILED 
+    else
+      testCaseStatus=PASSED
+    fi
+
+  else
+    # Since critical test failed before (testSuiteStatus = 1) - Skip this command
+    cmd="echo 'Critical test failed before - Skipping: ${cmd}'"
+    eVal "${cmd}"
+    returnCode=0
+    testCaseStatus=SKIP
+  fi
+
 
   # Save datetime after executing the command
   testEndTime="$(${dateTime} +%s.%N)"
@@ -197,17 +226,21 @@ EOF
   excapeXML "${outf}" || :
   excapeXML "${errf}" || :
 
-  # Set the appropriate error, based in the exit code and the regex
-  [[ "${returnCode}" != 0 ]] && testStatus=FAILED || testStatus=PASSED
-  # echo "+++ sh2ju exit code: ${returnCode} (testStatus=$testStatus)"
-  if [[ ${testStatus} == PASSED ]] && [[ -n "${ereg:-}" ]]; then
-      if grep -q -E ${icase} "${ereg}" "${outf}" ; then
-        testStatus=FAILED
-      fi
-  elif [[ ${testStatus} == FAILED ]] ; then
+  
+  if [[ ${testCaseStatus} == PASSED ]] && [[ -n "${ereg:-}" ]]; then
+    # Change testCaseStatus to FAILED if output includes regex of potential error
+    if grep -q -E ${icase} "${ereg}" "${outf}" ; then
+      testCaseStatus=FAILED
+    fi
+  elif [[ ${testCaseStatus} == FAILED ]] ; then
+    # Increase the failures + 1
     failures=$((failures+1))
+  elif [[ ${testCaseStatus} == SKIP ]] ; then
+    # Increase the skipped + 1
+    skipped=$((skipped+1))
   fi
 
+  
   # Calculate test duration and counter
   asserts=$((asserts+1))
   testDuration=$(echo "${testEndTime} ${testStartTime}" | awk '{print $1 - $2}')
@@ -232,31 +265,30 @@ EOF
 EOF
 
   # system-out tag if testcase passed
-  if [[ ${testStatus} == PASSED ]] ; then
-    # echo '    <system-out> <![CDATA[' >> ${newTestCaseTag}
-    # cat ${outf} >> ${newTestCaseTag}
-    # echo '    ]]> </system-out>' >> ${newTestCaseTag}
+  if [[ ${testCaseStatus} == PASSED ]] ; then
     echo '    <system-out>' >> "${newTestCaseTag}"
     cat "${outf}" >> "${newTestCaseTag}"
     echo '    </system-out>' >> "${newTestCaseTag}"
 
-  # Or failure tag if testcase failed
+  # Or skipped tag if testcase was skipped
+  elif [[ ${testCaseStatus} == SKIP ]] ; then
+    echo '    <skipped>' >> "${newTestCaseTag}"
+    cat "${outf}" >> "${newTestCaseTag}"
+    echo '    </skipped>' >> "${newTestCaseTag}"
+
+  # Or failure and error tags if testcase failed
   else
     # Get failure summary from $errf, by removing empty lines from $errf + getting last 2 lines
     # failure_summary=$(grep "\S" "$errf" | tail -2 | sed -e "s/\"/'/g" -e 's/&/\&amp;/g' -e 's/</\&lt;/g')
     failure_summary=$(grep "\S" "$errf" | tail -2)
 
-    # echo "    <failure type=\"ScriptError\" message=\"${failure_summary}\"> <![CDATA[" >> ${newTestCaseTag}
     echo "    <failure type=\"ScriptError\" message=\"${failure_summary}\">" >> "${newTestCaseTag}"
     cat "${outf}" >> "${newTestCaseTag}"
-    # echo '    ]]> </failure>' >> ${newTestCaseTag}
     echo '    </failure>' >> "${newTestCaseTag}"
 
     ## system-err tag in addition to failure tag
-    # echo '    <system-err> <![CDATA[' >> ${newTestCaseTag}
     echo '    <system-err>' >> "${newTestCaseTag}"
     cat "${errf}" >> "${newTestCaseTag}"
-    # echo '    ]]> </system-err>' >> ${newTestCaseTag}
     echo '    </system-err>' >> "${newTestCaseTag}"
 
   fi
@@ -265,38 +297,44 @@ EOF
   echo '      </testcase>' >> "${newTestCaseTag}"
 
   # Testsuite block
-  if [[ -e "${juDIR}/${juFILE}" ]]; then
+  if [[ -e "${juDir}/${juFile}" ]]; then
 
     # Get the number of failures in existing junit.xml, and append current failures counter to it
-    failCount=$(grep -Po -m1 'testsuite.*failures="\K[0-9]+' "${juDIR}/${juFILE}")
+    failCount=$(grep -Po -m1 'testsuite.*failures="\K[0-9]+' "${juDir}/${juFile}")
     [[ ! "$failCount" =~ ^[0-9]+$ ]] || failures=$((failCount+failures))
 
-    # Update the number of failures and errors in testsuite tag
-    ${SED} -i "0,/failures=\"${failCount}\"/ s/failures=\"${failCount}\"/failures=\"${failures}\"/" "${juDIR}/${juFILE}"
-    ${SED} -i "0,/errors=\"${failCount}\"/ s/errors=\"${failCount}\"/errors=\"${failures}\"/" "${juDIR}/${juFILE}"
+    # Get the number of skipped in existing junit.xml, and append current skipped counter to it
+    skipCount=$(grep -Po -m1 'testsuite.*skipped="\K[0-9]+' "${juDir}/${juFile}")
+    [[ ! "$skipCount" =~ ^[0-9]+$ ]] || skipped=$((skipCount+skipped))
+
+    # Update the number of failures (and errors the same), and number of skipped tests, in the testsuite tag
+    ${SED} -i "0,/failures=\"${failCount}\"/ s/failures=\"${failCount}\"/failures=\"${failures}\"/" "${juDir}/${juFile}"
+    ${SED} -i "0,/errors=\"${failCount}\"/ s/errors=\"${failCount}\"/errors=\"${failures}\"/" "${juDir}/${juFile}"
+    ${SED} -i "0,/skipped=\"${skipCount}\"/ s/skipped=\"${skipCount}\"/skipped=\"${skipped}\"/" "${juDir}/${juFile}"
 
     # In order to append the new testcase tag in the testsuite, remove the closing testsuite and testsuites end tags
-    ${SED} -i "s^</testsuite>^^g" "${juDIR}/${juFILE}"
-    ${SED} -i "s^</testsuites>^^g" "${juDIR}/${juFILE}"
+    ${SED} -i "s^</testsuite>^^g" "${juDir}/${juFile}"
+    ${SED} -i "s^</testsuites>^^g" "${juDir}/${juFile}"
 
     # Append the new testcase tag
-    cat "${newTestCaseTag}" >> "${juDIR}/${juFILE}"
+    cat "${newTestCaseTag}" >> "${juDir}/${juFile}"
 
     # Testsuite (and testsuites) tags end
-    cat <<-EOF >> "${juDIR}/${juFILE}"
+    cat <<-EOF >> "${juDir}/${juFile}"
     </testsuite>
 </testsuites>
 EOF
 
     # Update suite summary on the first <testsuite> tag:
     ${SED} -e "0,/<testsuite .*>/s/<testsuite .*>\s*/\
-    <testsuite name=\"${suiteTitle}\" tests=\"${testIndex}\" assertions=\"${assertions:-}\" failures=\"${failures}\" errors=\"${failures}\" time=\"${suiteDuration}\">/" \
-    -i "${juDIR}/${juFILE}"
+    <testsuite name=\"${suiteTitle}\" tests=\"${testIndex}\" assertions=\"${assertions:-}\" \
+    failures=\"${failures}\" errors=\"${failures}\" skipped=\"${skipped}\" time=\"${suiteDuration}\">/" \
+    -i "${juDir}/${juFile}"
 
   fi
 
-  # Set returnCode=0, if missing or equals 5
-  if [[ -z "$returnCode" ]] || [[ "$returnCode" == 5 ]] ; then
+  # Set returnCode=0: if using flag -status="StatusFile", or if returnCode was either not set or equals 5
+  if [[ -f "$statusFile" || -z "$returnCode" || "$returnCode" == 5 ]] ; then
     echo "+++ sh2ju re-setting return code: [${returnCode}] => [0]"
     returnCode=0
   else
